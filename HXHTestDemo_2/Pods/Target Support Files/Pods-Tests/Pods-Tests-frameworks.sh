6dff8a991e1aa043d52368c1eda8a342f044e59b
张强 <zq252125@163.com>
2017-03-05 17:52:17 +0800

    update GHUnit test

--- HXHTestDemo_2/Pods/Target Support Files/Pods-Tests/Pods-Tests-frameworks.sh
+++ HXHTestDemo_2/Pods/Target Support Files/Pods-Tests/Pods-Tests-frameworks.sh
@@ -82,10 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/GHUnit/GHUnit.framework"
-fi
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/GHUnit/GHUnit.framework"
-fi
--- HXHTestDemo_2/Tests/MyTest.m
+++ HXHTestDemo_2/Tests/MyTest.m
@@ -9,7 +9,14 @@
 @implementation MyTest
 
 - (void)test {
+    
+}
 
+- (void)testString {
+    NSString * string1 = @"a string";
+    GHTestLog(@"I can log to the GHUnit test console: %@", string1);
+    NSString * string2 = @"a string 1";
+    GHAssertEqualObjects(string1, string2, @"A custom error message. string1 should be equal to: %@.",string2);
 }
 
 @end
